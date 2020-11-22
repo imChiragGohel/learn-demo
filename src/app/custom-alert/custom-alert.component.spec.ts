79a9fb7b0980330afbfd78a921390567c89175cd
unknown <chirag94209@gmail.com>
2020-11-22 20:27:47 +0530

    Update NPM

--- src/app/custom-alert/custom-alert.component.spec.ts
+++ src/app/custom-alert/custom-alert.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomAlertComponent } from './custom-alert.component';
 
@@ -6,7 +6,7 @@ describe('CustomAlertComponent', () => {
   let component: CustomAlertComponent;
   let fixture: ComponentFixture<CustomAlertComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomAlertComponent ]
     })
